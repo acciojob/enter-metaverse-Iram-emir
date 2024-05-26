3b4a4f4acf4b2a07460f986154388bb3d1c2906c
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-26 18:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let a = document.getElementById('enterBtn');
+a.addEventListener('click',()=>{
+	let b = document.getElementById('status')
+	b.innerHTML = '<h1>Entered Metaverse</h1>'
+})
